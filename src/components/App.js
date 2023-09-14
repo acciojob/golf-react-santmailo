bafb25d4b11b81d8b8032d69ecf4e10c43b7b011
santmailo <iamsunil006@gmail.com>
2023-09-14 18:33:12 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
         this.state = {
             renderBall: false,
             posi : 0,
-            ballPosition: { left: `0px` }    
+            ballPosition: { left: `5px` }    
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
